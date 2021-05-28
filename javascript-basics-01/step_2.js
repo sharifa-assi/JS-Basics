010239246e3a7a37816790e34b49cfbf914767f5
sharifa-assi <sharifaalassi2001@gmail.com>
2021-05-28 15:43:36 +0300

    Variables 2

---
+++ javascript-basics-01/step_2.js
@@ -0,0 +1,10 @@
+
+
+function albox(){
+    var name = "sharifa";
+    var surname = "alassi";
+    var city = "Beirut";
+
+    window.alert("name : " + name +"\n" +"surname : " + surname+ "\n"+"city : " + city);
+   
+}
